17773427593e844e46b1c0e19db4977e8caf5932
dimakolbasin <dimakolbasin98@gmail.com>
2026-02-04 00:40:24 +0400

    add icons

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
   eleventyConfig.addPassthroughCopy({ "src/styles.css": "styles.css" });
+  eleventyConfig.addPassthroughCopy({ "src/favicon.ico": "favicon.ico" });
 
   return {
     dir: {
