8d460e791af152feab903b5c592e08eacc33ca42
dimakolbasin <dimakolbasin98@gmail.com>
2026-02-04 01:51:56 +0400

    add env

---
+++ src/_data/site.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const rawPhone = process.env.SITE_PHONE || "";
+// Убираем всё, кроме цифр и плюса. Используем этот формат и для ссылок, и для отображения.
+const cleanPhone = rawPhone.replace(/[^0-9+]/g, '');
+
+module.exports = {
+    name: process.env.SITE_NAME,
+    phone: cleanPhone,
+    email: process.env.SITE_EMAIL,
+    baseUrl: process.env.SITE_BASE_URL,
+    facebook: process.env.SITE_FACEBOOK,
+    telegram: process.env.SITE_TELEGRAM,
+    formSubmitEmail: process.env.FORMSUBMIT_EMAIL
+};
